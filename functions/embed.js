02a545544234dfdd3a0b0168ee9e479448fa6a4e
Alexander Paz <alexjpaz@gmail.com>
2022-02-02 07:18:59 -0500

    embed

--- functions/embed.js
+++ functions/embed.js
@@ -3,7 +3,11 @@ exports.handler = async function (event, context) {
   <html>
     <head>
       <title>Earworm</title>
-      <meta content="${event.queryStringParameters.alert}" property="og:title" />
+      <meta content="Earworm: Playground" property="og:title" />
+      <meta content="!${decodeURIComponent(event.queryStringParameters.alert)}" Description" property="og:description" />
+      <meta property="og:footer" content="Click to play alert">
+
+
     </head>
     <body>
       <script>location.assign("https://dhttm9zkbfvzw.cloudfront.net/alexjpaz-twitch/alerts/playground.html?alert=${event.queryStringParameters.alert}")</script>
